5b4fdf873bfd48620ce93b378a8e3b5b471adb8b
Svend Sorensen <svend@svends.net>
2019-02-26 20:18:17 -0800

    Update

--- src/day8.rs
+++ src/day8.rs
@@ -1,29 +1,64 @@
-fn process_tree(ns: &[usize], meta2: &[usize]) -> (usize, Vec<usize>) {
-    let mut meta2: Vec<_> = meta2.into_iter().cloned().collect();
-    dbg!(&meta2);
+#[derive(Debug)]
+struct Node {
+    meta: Vec<usize>,
+    children: Vec<Node>,
+}
 
+impl Node {
+    fn new() -> Node {
+        Node {
+            meta: vec![],
+            children: vec![],
+        }
+    }
+
+    fn len(&self) -> usize {
+        let mut len = 0;
+        for child in &self.children {
+            len += child.len();
+        }
+        2 + len + self.meta.len()
+    }
+
+    fn checksum(&self) -> usize {
+        let mut checksum = self.meta.iter().sum();
+        for child in &self.children {
+            checksum += child.checksum();
+        }
+        checksum
+    }
+
+    fn checksum_2(&self) -> usize {
+        if self.children.is_empty() {
+            self.meta.iter().sum()
+        } else {
+            let mut checksum = 0;
+            for i in &self.meta {
+                let idx = *i - 1;
+                if let Some(child) = self.children.get(idx) {
+                    checksum += child.checksum_2()
+                }
+            }
+            checksum
+        }
+    }
+}
+
+fn process_tree(ns: &[usize]) -> Node {
+    let mut node = Node::new();
     let num_children = ns[0];
     let num_meta = ns[1];
 
-    let mut len_children = 0;
+    let mut children_end = 2;
     for _ in 0..num_children {
-        let (x, meta) = process_tree(&ns[(2 + len_children)..], &vec![]);
-        meta2.extend(meta);
-        len_children += x;
+        let child = process_tree(&ns[children_end..]);
+        children_end += child.len();
+        node.children.push(child);
     }
-    let meta = &ns[2 + len_children..2 + len_children + num_meta];
-    meta2.extend(meta);
+    let meta = &ns[children_end..children_end + num_meta];
+    node.meta.extend(meta);
 
-    dbg!(num_children);
-    dbg!(num_meta);
-    dbg!(meta);
-    (2 + len_children + num_meta, meta2)
-}
-
-fn process_data(ns: &[usize]) -> usize {
-    let (_, meta) = process_tree(&ns, &vec![]);
-    dbg!(&meta);
-    meta.into_iter().sum()
+    node
 }
 
 fn parse_input(s: &str) -> Vec<usize> {
@@ -37,13 +72,24 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() {
+    fn test_checksum() {
         let input = include_str!("8.example.input");
         let nums = parse_input(&input);
-        assert_eq!(process_data(&nums), 138);
+        assert_eq!(process_tree(&nums).checksum(), 138);
 
-        // let input = include_str!("8.input");
-        // let nums = parse_input(&input);
-        // assert_eq!(process_data(&nums), 0);
+        let input = include_str!("8.input");
+        let nums = parse_input(&input);
+        assert_eq!(process_tree(&nums).checksum(), 46781);
+    }
+
+    #[test]
+    fn test_checksum_2() {
+        let input = include_str!("8.example.input");
+        let nums = parse_input(&input);
+        assert_eq!(process_tree(&nums).checksum_2(), 66);
+
+        let input = include_str!("8.input");
+        let nums = parse_input(&input);
+        assert_eq!(process_tree(&nums).checksum_2(), 21405);
     }
 }
